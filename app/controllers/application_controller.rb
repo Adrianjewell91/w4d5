74fcf87919adb7ab52c3291183c854aa101eaa29
Adrian <adrianjewell91@gmail.com>
2017-08-25 15:40:37 -0700

    add buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
-  helper_method :logged_in?
-  helper_method :require_logged_in
+  helper_method :logged_in? #makes it available to the view.
 
   def logged_in?
     current_user != nil
